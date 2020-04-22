1ea20020e0233162233d33ff4af41ed4c04278d0
panh1992 <panh1992@163.com>
2020-04-22 22:30:17 +0800

    权限认证修改

--- gateway/src/main/java/org/gateway/security/AuthorizationDeniedEntryPoint.java
+++ gateway/src/main/java/org/gateway/security/AuthorizationDeniedEntryPoint.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
@@ -24,10 +23,9 @@ public class AuthorizationDeniedEntryPoint implements ServerAuthenticationEntryP
     @Override
     public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException denied) {
         ServerHttpResponse response = exchange.getResponse();
-        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
-        InetSocketAddress remoteAddress = exchange.getRequest().getRemoteAddress();
-        String clientIp = Objects.requireNonNull(remoteAddress).getAddress().getHostAddress();
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        String clientIp = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress();
         ErrorResp errorResp = ErrorResp.builder().hostId(clientIp).requestId(exchange.getRequest().getId())
                 .code(HttpStatus.UNAUTHORIZED.name()).message(denied.getMessage()).build();
         return response.writeWith(Mono.just(response.bufferFactory().wrap(CommonUtil.toJson(errorResp)
